fe358672a2b6916b9b8beee56a5c14cd63fb22b8
RakaRizkya <rakarizkya07@gmail.com>
2022-10-24 16:00:43 +0700

    hari pertama

---
+++ sintaks.php
@@ -0,0 +1,70 @@
+<?php
+// Ini Komentar
+/* Ini Juga 
+Komentar */
+
+// Standar Output : echo, print, print_r, var_dump
+
+//echo "Hello World";
+//print "Hello World";
+//print_r("Hello World");
+// var_dump("Hello World");
+
+// $nama = "Raka";
+// echo "Nama saya $nama";
+// echo "<br>";
+// echo 'Nama saya $nama';
+// echo "<br>";
+
+ 
+// Operator + - / * %
+// $angka1 = 10;
+// $angka2 = 20;
+// echo $angka1 + $angka2;
+// echo "<br>";
+ 
+// Concat .
+// $nama_depan = "Raka";
+// $nama_belakang = "Rizkya";
+// echo $nama_depan . " Rizkya <br>";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment = += -= *= /= %= .=
+// $m = 1;
+// $m .= 2;
+// echo $m;
+
+// Perbandingan <, >, <=, >=, ==, !=
+// var_dump(1 == 5);
+// var_dump(1 == "1");
+
+//Identitas : ===, !==
+// var_dump(1 === "1");
+
+// Logika : AND (&&), OR(||), NOT (!)
+var_dump(1<5 || 2>3);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+    <h1> 
+        <?php 
+            // $a = "Andi";
+            // echo $a; 
+        ?>
+    </h1>
+    <br>
+    <?php 
+        // $x = "Doni";
+        // echo "<h1>Selamat datang $x</h1>";
+    ?>
+</body>
+</html>
